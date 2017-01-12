38b76727ebef99fa6401e48c81f88f41996259a3
Eleanor Kavanagh-Brown <ekavanaghbrown@gmail.com>
2017-01-12 16:31:44 +0000

    add journey history

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,13 @@
 class Oystercard
 MAX_BALANCE = 90
 MIN_FARE = 1
-attr_reader :balance, :in_journey, :entry_station
+attr_reader :balance, :in_journey, :entry_station, :journeys
 
   def initialize(balance = 0)
     @in_journey = false
     @balance = balance
     @entry_station = nil
+    @journeys = []
   end
 
   def top_up(amount)
@@ -20,13 +21,14 @@ attr_reader :balance, :in_journey, :entry_station
     @entry_station = station
   end
 
-  def touch_out
+  def touch_out(station)
     deduct(MIN_FARE)
+    @journeys << { entry_station: entry_station, exit_station: station }
     @entry_station = nil
   end
 
   def in_journey?
-    @in_journey = true unless @entry_station == nil
+    !!entry_station
   end
 
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,7 +6,8 @@ subject(:oystercard) { described_class.new }
 subject(:oystercard2) { described_class.new 10 }
 MAX_BALANCE = 90
 MIN_FARE = 1
-let(:station) { double :station }
+let(:entry_station) { double :station1 }
+let(:exit_station) { double :station2 }
 
 
   context "Freshly initialized card" do
@@ -35,36 +36,42 @@ let(:station) { double :station }
   context '#touch_in' do
 
     it "records entry_station" do
-      subject.touch_in(station)
-      expect(subject.entry_station).to eq station
+      subject.touch_in(entry_station)
+      expect(subject.entry_station).to eq entry_station
     end
 
     it "changes status of in_journey? to true" do
-      subject.touch_in(station)
+      subject.touch_in(entry_station)
       expect(subject).to be_in_journey
     end
     it "raises error if not enough money" do
-      expect{ oystercard.touch_in(station) }.to raise_error "Not enough money."
+      expect{ oystercard.touch_in(entry_station) }.to raise_error "Not enough money."
     end
   end
 
   context '#touch_out' do
     before do
-      subject.touch_in(station)
+      subject.touch_in(entry_station)
     end
 
     it "changes status of in_journey? to false" do
-      subject.touch_out
+      subject.touch_out(exit_station)
       expect(subject).not_to be_in_journey
     end
 
     it "deducts correct fare from balance" do
-      expect { subject.touch_out }.to change { subject.balance }.by -MIN_FARE
+      expect { subject.touch_out(exit_station) }.to change { subject.balance }.by -MIN_FARE
     end
 
     it "sets the entry_station to nil upon touch_out" do
-      subject.touch_out
+      subject.touch_out(exit_station)
       expect(subject.entry_station).to eq nil
+    end
+
+    it 'stores entry_station and exit_station in array of past journeys' do
+      #subject.touch_in(station)
+      subject.touch_out(exit_station)
+      expect(subject.journeys).to include ({:entry_station=>entry_station, :exit_station=>exit_station})
     end
 
 
