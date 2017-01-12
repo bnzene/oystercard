9168652c3ac365fb5e96443953ebfbe20bea4336
Eleanor Kavanagh-Brown <ekavanaghbrown@gmail.com>
2017-01-12 12:43:29 +0000

    Add balance method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,10 @@
-describe Oystercard do
+require 'oystercard'
 
+describe Oystercard do
+  context "Freshly initialized card" do
+    it "Tests that a new card has a balance of 0 by default" do
+      subject = Oystercard.new
+      expect(subject.balance).to eq(0)
+    end
+  end
 end
